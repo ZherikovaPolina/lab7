0c2532561772ab397296056541f82cfce96bbb86
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-12 22:21:59 +0300

    Реалізовано сторінку перегляду деталей інвентарю з отриманням даних через API та відображенням станів завантаження і помилок

---
+++ src/pages/AdminInventoryDetails.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getInventoryById } from "../services/inventoryApi";
+import InventoryDetails from "../components/inventory/InventoryDetails";
+
+export default function AdminInventoryDetails() {
+  const { id } = useParams();
+  const [item, setItem] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      try {
+        const data = await getInventoryById(id);
+        setItem(data);
+      } catch (e) {
+        setError("Помилка завантаження даних");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchItem();
+  }, [id]);
+
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h1>Деталі інвентарю</h1>
+
+      <InventoryDetails item={item} />
+    </div>
+  );
+}
