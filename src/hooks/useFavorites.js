1dcc94a3cdc594fb593c2e96a29c2b61fb2683f8
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-19 23:35:54 +0300

    Створено кастомний хук useFavorites для доступу до FavoritesContext та централізованого управління улюбленими

--- src/hooks/useFavorites.js
+++ src/hooks/useFavorites.js
@@ -1,29 +1,12 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/FavoritesContext";
 
 export default function useFavorites() {
-  const [favorites, setFavorites] = useState([]);
+  const context = useContext(FavoritesContext);
 
-  useEffect(() => {
-    const saved = JSON.parse(localStorage.getItem("favorites")) || [];
-    setFavorites(saved);
-  }, []);
+  if (!context) {
+    throw new Error("useFavorites must be used inside FavoritesProvider");
+  }
 
-  const toggleFavorite = (item) => {
-    let updated;
-
-    if (favorites.find((f) => f.id === item.id)) {
-      updated = favorites.filter((f) => f.id !== item.id);
-    } else {
-      updated = [...favorites, item];
-    }
-
-    setFavorites(updated);
-    localStorage.setItem("favorites", JSON.stringify(updated));
-  };
-
-  const isFavorite = (id) => {
-    return favorites.some((f) => f.id === id);
-  };
-
-  return { favorites, toggleFavorite, isFavorite };
+  return context;
 }
