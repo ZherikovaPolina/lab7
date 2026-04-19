1880e487a153dbeb36c73bb0bcfc93af9349abd6
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-19 23:35:46 +0300

    Створено FavoritesContext: реалізовано глобальний стан улюблених елементів, додано логіку додавання/видалення та синхронізацію з localStorage

---
+++ src/store/FavoritesContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState, useEffect } from "react";
+
+export const FavoritesContext = createContext();
+
+export function FavoritesProvider({ children }) {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(data);
+  }, []);
+
+  const toggleFavorite = (item) => {
+    let updated;
+
+    if (favorites.some((f) => f.id === item.id)) {
+      updated = favorites.filter((f) => f.id !== item.id);
+    } else {
+      updated = [...favorites, item];
+    }
+
+    setFavorites(updated);
+    localStorage.setItem("favorites", JSON.stringify(updated));
+  };
+
+  return (
+    <FavoritesContext.Provider value={{ favorites, toggleFavorite }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+}
