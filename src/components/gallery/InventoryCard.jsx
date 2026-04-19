92aedee362d018b3775a5c0f71cdddd7dc1e0d62
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-19 21:19:36 +0300

    Створено компонент InventoryCard для відображення окремого елемента інвентарю: реалізовано відображення зображення, назви та кнопку додавання в улюблені, додано обробку кліку для відкриття детального перегляду та запобігання спливанню подій

---
+++ src/components/gallery/InventoryCard.jsx
@@ -0,0 +1,18 @@
+export default function InventoryCard({ item, onClick, toggleFavorite, isFavorite }) {
+  return (
+    <div className="card" onClick={() => onClick(item)}>
+      <img src={item.photo} alt="" />
+      <h3>{item.inventory_name}</h3>
+
+      <button
+        className="fav-btn"
+        onClick={(e) => {
+          e.stopPropagation();
+          toggleFavorite(item);
+        }}
+      >
+        {isFavorite(item.id) ? "❤️" : "🤍"}
+      </button>
+    </div>
+  );
+}
