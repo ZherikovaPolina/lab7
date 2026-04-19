8425f9d4cd8b958d2d82bbcd02802b1d005c6b26
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-19 23:36:16 +0300

    Підключено FavoritesProvider у main.jsx для глобального доступу до стану улюблених у всьому застосунку

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
 import { InventoryProvider } from "./store/InventoryContext";
+import { FavoritesProvider } from "./store/FavoritesContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <InventoryProvider>
-    <App />
+    <FavoritesProvider>
+      <App />
+    </FavoritesProvider>
   </InventoryProvider>
 );
